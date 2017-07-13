967519bf33ef030ff5a1ccaf172ccfc14f87f4fd
vostokova <nbvostokova@gmail.com>
2017-07-13 15:47:38 +0300

    some changes

--- src/ts.ts
+++ src/ts.ts
@@ -1,104 +1,129 @@
-﻿const vehicles = [];
-const button = document.getElementById('button');
-const list = document.getElementById('vehicle-list');
+﻿let vehicles: Vehicle[];
+let byID = (id:string):HTMLElement => document.getElementById();
+let button:HTMLElement = byID('button');
+let list:HTMLElement = byID('list');
+let options:HTMLElement[]= list.childNodes;
 
-button.onclick = function() {
-    let vehicle;
-    let type;
-    let name = document.getElementById('name');
-    let perf = document.getElementById('performance');
-    let description = document.getElementById('description');
+abstract class Vehicle {
+    name:string;
+    type:string;
+    description?:string;
+    constructor (name:string, type:string, desc?:string) {
+        this.name = name;
+        this.type = type;
+        this.description = desc;
+    }
+    showLabel():string {
+        return `${this.name} ${this.customLabel()}`;
+    }
+    showInfo():string {
+        return `Тип: ${this.type}<br>
+            Модель: ${this.name}<br>
+            ${this.customInfo()}<br>
+            Примечание: ${this.description}`;
+    }
+    abstract customLabel():string;
+    abstract customInfo():string;
+}
+
+class Truck extends Vehicle {
+    carrying:number;
+    constructor (name:string, type:string, carrying:number, desc?:string) {
+        super (name, type, desc);
+        this.carrying = carrying;
+    }
+    customLabel():string {
+        return `${this.carrying} т`;
+    }
+    customInfo():string {
+        return `Масса: ${this.carrying} т`;
+    }
+}
+
+class Car extends Vehicle {
+    velocity:number;
+    constructor (name:string, type:string, velocity:number, desc?:string) {
+        super (name, type, desc);
+        this.velocity = velocity;
+    }
+    showLabel():string {
+        return `${this.velocity} км/ч`;
+    }
+    showInfo():string {
+        return `Скорость: ${this.velocity} км/ч`;
+    }
+}
+
+button.onclick = function():void {
+    let vehicle:Vehicle;
+    let nameField:HTMLElement = byID('name');
+    let perf:HTMLElement = byID('performance');
+    let desc:HTMLElement = byID('description');
+
     let radios = document.getElementsByName('type');
-    let checkType = () => { //TODO: assign to type, add let 'value', return value
-        for (let i=0; i<radios.length; i++) {
-            if (radios[i].checked) type = (radios[i].value);
-        }
-    };
-    checkType();
-    let validateFields = () => {
-        errorMessage = '';
+
+    function checkType():string {
+        for (let i=0; i<radios.length; i++)
+            if (radios[i].checked) return radios[i].nodeValue;
+    }
+
+    function validateFields():string {
+        let errorMessage = '';
         if (!type) errorMessage += 'Выберите тип! ';
-        if (!name.value) errorMessage += 'Введите название! ';
-        if (!perf.value) errorMessage += 'Введите характеристику автомобиля!';
+        if (!nameField.nodeValue) errorMessage += 'Введите название! ';
+        if (!perf.nodeValue) errorMessage += 'Введите характеристику автомобиля!';
         return errorMessage;
-    };
-    let errorMessage = validateFields();
-    if (errorMessage) alert (errorMessage);
-    else {
-        switch (type) {
-            case 'truck':
-                vehicle = new Truck(name.value, description.value, perf.value);
-                break;
-            case 'car':
-                vehicle = new Car(name.value, description.value, perf.value);
-                break;
+    }
+
+    function clearForm():void {
+        type = null;
+        nameField.nodeValue = null;
+        perf.nodeValue = null;
+        desc.nodeValue = null;
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].checked) radios[i].checked = false;
         }
-        vehicles.push({
-            vehicle: vehicle,
-            onClick: vehicle.showInfo.bind(vehicle)
-        });
-        list.options[list.options.length] = new Option(vehicle.showLabel());
-        let clearForm = () => {
-            type = null;
-            name.value = null;
-            perf.value = null;
-            description.value = null;
-            for (let i=0; i<radios.length; i++) {
-                if (radios[i].checked) radios[i].checked = false;
-            }
-            document.getElementById('perf-label').innerHTML = 'Macca/скорость*';
-        };
-        clearForm();
+//        byID('perf-label').innerHTML = 'Macca/скорость*';
     }
-};
 
-function Vehicle (name, description) {
-    // сохранение параметров name, description
-    this.name = name;
-    this.description = description;
+    let type:string = checkType();
+    let errorMessage:string = validateFields();
+    if (errorMessage) {
+        alert(errorMessage);
+        return;
+    }
+    switch (type): {
+    case 'truck':
+        vehicle = new Truck(nameField.nodeValue, type, perf.nodeValue, desc.nodeValue);
+        break;
+    case 'car':
+        vehicle = new Car(nameField.nodeValue, type, perf.nodeValue, desc.nodeValue);
+        break;
+    }
+    vehicles.push({
+        vehicle: vehicle;
+        onClick: vehicle.showInfo.bind(vehicle)
+    });
+    options[options.length] = '<option>${vehicle.showLabel()}</option>';
+    clearForm();
 }
 
-function Truck (name, description, perf) {
-    Vehicle.apply(this, arguments);
-    this.type = "грузовая";
-    this.carrying = perf;
 
-    // информация для отображения в списке машин
-    this.showLabel = function () {return this.name + ": " + this.carrying + " т"};
-    // информация для отображения под списком машин, при выборе строки в списке
-    this.showInfo = function () {
-        return "Тип: " + this.type + "<br>" +
-            "Модель: " + this.name + "<br>" +
-            "Масса: " + this.carrying + "<br>" +
-            "Примечание: " + this.description;
+list.onclick = function():void {
+    if (list.s != -1) {
+        document.getElementById('show-info').innerHTML = vehicles[list.selectedIndex].onClick();
     }
 }
-
-function Car (name, description, perf) {
-    Vehicle.apply(this, arguments);
-    this.type = "легковая";
-    this.velocity = perf;
-
-    // информация для отображения в списке машин
-    this.showLabel = function () {return this.name + ": " + this.velocity + " км/ч"};
-    // информация для отображения под списком машин, при выборе строки в списке
-    this.showInfo = function () {
-        return "Тип: " + this.type + "<br>" +
-            "Модель: " + this.name + "<br>" +
-            "Масса: " + this.velocity + "<br>" +
-            "Примечание: " + this.description;
-    }
-}
-
+/*
 list.onclick = function () {
     if (list.selectedIndex != -1) {
         document.getElementById('show-info').innerHTML = vehicles[list.selectedIndex].onClick();
     }
 };
 
-document.getElementById('truck').onclick = function() {
+document.getElementById('truck').onclick = function () {
     document.getElementById('perf-label').innerHTML = 'Масса*';
 };
-document.getElementById('car').onclick = function() {
+document.getElementById('car').onclick = function () {
     document.getElementById('perf-label').innerHTML = 'Скорость*';
-};
+};*/
