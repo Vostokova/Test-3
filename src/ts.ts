25c0185e3fabd27a2b63590d277120d0ea70cbb0
vostokova <nbvostokova@gmail.com>
2017-07-15 00:25:23 +0300

    fixed select options, reviewed onclick functions

--- src/ts.ts
+++ src/ts.ts
@@ -1,17 +1,17 @@
-﻿let vehicles: Vehicle[];
-let byID = (id:string):HTMLElement => document.getElementById();
+﻿let vehicles[];
+let byID = (id:string):HTMLElement => document.getElementById(id);
 let button:HTMLElement = byID('button');
-let list:HTMLElement = byID('list');
-let options:NodeList = list.childNodes;
+let list:HTMLElement = byID('vehicle-list');
+let options:NodeListOf<HTMLElement> = list.options;
 
-abstract class Vehicle {
+class Vehicle {
     name:string;
     type:string;
     description?:string;
-    constructor (name:string, type:string, desc?:string) {
+    constructor (name:string, type:string, description?:string) {
         this.name = name;
         this.type = type;
-        this.description = desc;
+        this.description = description;
     }
     showLabel():string {
         return `${this.name} ${this.customLabel()}`;
@@ -28,8 +28,8 @@ abstract class Vehicle {
 
 class Truck extends Vehicle {
     carrying:number;
-    constructor (name:string, type:string, carrying:number, desc?:string) {
-        super (name, type, desc);
+    constructor (name:string, type:string, carrying:number, description?:string) {
+        super (name, type, description);
         this.carrying = carrying;
     }
     customLabel():string {
@@ -42,88 +42,82 @@ class Truck extends Vehicle {
 
 class Car extends Vehicle {
     velocity:number;
-    constructor (name:string, type:string, velocity:number, desc?:string) {
-        super (name, type, desc);
+    constructor (name:string, type:string, velocity:number, description?:string) {
+        super (name, type, description);
         this.velocity = velocity;
     }
-    showLabel():string {
+    customLabel():string {
         return `${this.velocity} км/ч`;
     }
-    showInfo():string {
+    customInfo():string {
         return `Скорость: ${this.velocity} км/ч`;
     }
 }
 
-button.onclick = function():void {
+button.onclick = () => {
     let vehicle:Vehicle;
     let nameField:HTMLElement = byID('name');
-    let perf:HTMLElement = byID('performance');
-    let desc:HTMLElement = byID('description');
+    let performance:HTMLElement = byID('performance');
+    let description:HTMLElement = byID('description');
+    let radios:NodeListOf<HTMLElement> = document.getElementsByName('type');
+    let type:string;
 
-    let radios = document.getElementsByName('type');
-
-    function checkType():string {
+    function checkType():void {
         for (let i=0; i<radios.length; i++)
-            if (radios[i].checked) return radios[i].nodeValue;
+            if (radios[i].checked) type = radios[i].value;
     }
 
     function validateFields():string {
         let errorMessage = '';
         if (!type) errorMessage += 'Выберите тип! ';
-        if (!nameField.nodeValue) errorMessage += 'Введите название! ';
-        if (!perf.nodeValue) errorMessage += 'Введите характеристику автомобиля!';
+        if (!nameField.value) errorMessage += 'Введите название! ';
+        if (!performance.value) errorMessage += 'Введите характеристику автомобиля!';
         return errorMessage;
     }
 
     function clearForm():void {
         type = null;
-        nameField.nodeValue = null;
-        perf.nodeValue = null;
-        desc.nodeValue = null;
+        nameField.value = null;
+        performance.value = null;
+        description.value = null;
         for (let i = 0; i < radios.length; i++) {
             if (radios[i].checked) radios[i].checked = false;
         }
-//        byID('perf-label').innerHTML = 'Macca/скорость*';
+        byID('perf-label').innerHTML = 'Macca/скорость*';
     }
 
-    let type:string = checkType();
+    checkType();
     let errorMessage:string = validateFields();
     if (errorMessage) {
         alert(errorMessage);
         return;
     }
-    switch (type): {
-    case 'truck':
-        vehicle = new Truck(nameField.nodeValue, type, perf.nodeValue, desc.nodeValue);
-        break;
-    case 'car':
-        vehicle = new Car(nameField.nodeValue, type, perf.nodeValue, desc.nodeValue);
-        break;
+    switch (type) {
+        case 'truck':
+            vehicle = new Truck(nameField.value, 'грузовая', performance.value, description.value);
+            break;
+        case 'car':
+            vehicle = new Car(nameField.value, 'легковая', performance.value, description.value);
+            break;
     }
     vehicles.push({
-        vehicle: vehicle;
+        vehicle: vehicle,
         onClick: vehicle.showInfo.bind(vehicle)
     });
-    options[options.length] = '<option>${vehicle.showLabel()}</option>';
+    options[options.length] = new Option (vehicle.showLabel());
     clearForm();
-}
+};
 
 
 list.onclick = function():void {
-    if (list.s != -1) {
-        document.getElementById('show-info').innerHTML = vehicles[list.selectedIndex].onClick();
-    }
-}
-/*
-list.onclick = function () {
     if (list.selectedIndex != -1) {
         document.getElementById('show-info').innerHTML = vehicles[list.selectedIndex].onClick();
     }
 };
 
-document.getElementById('truck').onclick = function () {
-    document.getElementById('perf-label').innerHTML = 'Масса*';
+byID('truck').onclick = function ():void {
+    byID('perf-label').innerHTML = 'Масса*';
 };
-document.getElementById('car').onclick = function () {
-    document.getElementById('perf-label').innerHTML = 'Скорость*';
-};*/
+byID('car').onclick = function ():void {
+    byID('perf-label').innerHTML = 'Скорость*';
+};
